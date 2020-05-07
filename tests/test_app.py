85ad3312c506220fd223328c2b490d8b991d34f3
sept2 <62025009+sept2@users.noreply.github.com>
2020-05-07 17:28:40 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,9 +14,9 @@ def test_grid_analysis():
     for j in range(height):
         row = []
         for i in range(width):
-            row.append(covid_G2.generate_people_info())
+            row.append(app.generate_people_info())
         grid.append(row)
-    new_grid, victim_name, victim_count, vic_plot, time_step = covid_G2.grid_analysis(grid, height, width)
+    new_grid, victim_name, victim_count, vic_plot, time_step = app.grid_analysis(grid, height, width)
     def test_sub_1(new_grid, victim_name, victim_count, vic_plot, time_step):
         return True if new_grid != [] and victim_name != [] and victim_count  != 0 and vic_plot  != [] and time_step != 0 else False
     assert test_sub_1(new_grid, victim_name, victim_count, vic_plot, time_step) is True
@@ -28,7 +28,7 @@ def test_finding_victims():
     for j in range(height):
         row = []
         for i in range(width):
-            row.append(covid_G2.generate_people_info())
+            row.append(app.generate_people_info())
         grid.append(row)
     i = 0
     j = 0
@@ -37,7 +37,7 @@ def test_finding_victims():
     vic_plot = []
     victim_name = []
     adjacent_grid = [grid[i][j + 1], grid[i + 1][j], grid[i + 1][j + 1]]
-    victim_name, victim_count, vic_plot = covid_G2.finding_victims(adjacent_grid, victim_name, victim_count, time_step, vic_plot)
+    victim_name, victim_count, vic_plot = app.finding_victims(adjacent_grid, victim_name, victim_count, time_step, vic_plot)
     def test_sub_2( victim_name, victim_count, vic_plot):
         return True if victim_name != [] and victim_count != 0 and vic_plot != [] else False
 
